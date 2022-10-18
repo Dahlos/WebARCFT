a582c86ca3e6000832ae70f282f381d9a388233e
Dahlos <seba.manriquezh@gmail.com>
2022-10-18 18:59:42 -0300

    change animations

--- js/main.js
+++ js/main.js
@@ -1,21 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // const modelViewer = document.getElementById("printer-model");
-  // const modelViewer = document.querySelector("model-viewer#printer-model");
+(() => {
+  const modelViewer = document.querySelector('#printer-model');
 
-  playAnimations();
-});
-
-const playAnimations = () => {
-  let nextAnimation = "Cube.017Action";
-  const modelViewer = document.getElementById("printer-model");
-  modelViewer.setAttribute("animation-name", nextAnimation);
-  setInterval(() => {
-    modelViewer.setAttribute("animation-name", nextAnimation);
-    if (nextAnimation === "Cube.017Action") {
-      nextAnimation = "Cube.033Action";
-    } else {
-      nextAnimation = "Cube.017Action";
-    }
-    console.log(modelViewer.scale);
-  }, 6000);
-};
+  self.setInterval(() => {
+    modelViewer.animationName = modelViewer.animationName === 'Cylinder.014Action' ?
+      'Cube.033Action' : 'Cylinder.014Action';
+  }, 5500.0);
+})();
